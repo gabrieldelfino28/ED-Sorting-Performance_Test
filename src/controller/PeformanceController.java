bc8a7138b8ebe47f2b3d0aa90f74421aca3eec88
gabrieldelfino28 <gabrieldelfino28@gmail.com>
2023-09-02 18:24:20 -0300

    Testing Peformance

---
+++ src/controller/PeformanceController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import br.edu.fateczl.algoritmos.bubblesort.BubbleSort;
+import br.edu.fateczl.algoritmos.mergesort.MergeSort;
+import br.edu.fateczl.algoritmos.quicksort.QuickSort;
+
+public class PeformanceController {
+    public PeformanceController() {
+        super();
+    }
+
+    public String BubbleTest(int[] arr){
+        BubbleSort sort = new BubbleSort();
+
+        double start = System.nanoTime();
+        int[] sortedArr = sort.BubbleSort(arr);
+        double end = System.nanoTime();
+
+        double duration = (end - start);
+        duration /= Math.pow(10,9);
+        return "Bubble: "+duration+ " ms";
+    }
+
+    public String MergeTest(int[] arr){
+        MergeSort sort = new MergeSort();
+        int end = arr.length - 1;
+
+        double start = System.nanoTime();
+        int[] sortedArr = sort.MergeSort(arr,0,end);
+        double endT = System.nanoTime();
+
+        double duration = (endT - start);
+        duration /= Math.pow(10,9);
+        return "Merge: "+duration+ " ms";
+    }
+
+    public String QuickTest(int[] arr){
+        QuickSort sort = new QuickSort();
+        int end = arr.length - 1;
+
+        double start = System.nanoTime();
+        int[] sortedArr = sort.QuickSort(arr,0,end);
+        double endT = System.nanoTime();
+
+        double duration = (endT - start);
+        duration /= Math.pow(10,9);
+
+        return "Quick: "+duration+ " ms";
+    }
+}
---
+++ src/view/Main.java
@@ -0,0 +1,34 @@
+package view;
+
+import controller.PeformanceController;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PeformanceController p = new PeformanceController();
+        int[] Array = new int[1500];
+        int j = 0;
+
+        //Creating a long Array for testing.
+        for (int i = 1499; i >= 0; i--) {
+            Array[i] = j;
+            j++;
+        }
+
+        System.out.println(p.BubbleTest(Array));
+        System.out.println(p.MergeTest(Array));
+        System.out.println(p.QuickTest(Array));
+
+        //Testing with a random array with 10000 slots.
+        int[] arr = new int[10000];
+        for (int i = 0; i < 10000; i++) {
+            arr[i] = (int) (Math.random() * 9999 + 1);
+        }
+
+        System.out.println("With a random Array with 10000 slots");
+        System.out.println(p.BubbleTest(arr));
+        System.out.println(p.MergeTest(arr));
+        System.out.println(p.QuickTest(arr));
+    }
+}
